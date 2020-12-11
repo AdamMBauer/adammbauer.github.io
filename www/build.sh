89b5d1792b7e7022047bfc0f5a499122f9d01ec5
ElidorAzzeal <bauer.miles.adam@gmail.com>
2020-12-11 11:13:52 +0000

    set up for static header and footer and removed uneeded files implemented

---
+++ www/build.sh
@@ -0,0 +1,8 @@
+
+cat src/header.html > index.html
+cat src/home.html >> index.html
+cat src/footer.html >> index.html
+
+cat src/header.html > index.html
+cat src/cv.html >> index.html
+cat src/footer.html >> index.html
