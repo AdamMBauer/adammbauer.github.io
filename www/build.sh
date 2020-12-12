8f08ebb6f66b4986a82519c3ad1ca221eb99ac31
ElidorAzzeal <bauer.miles.adam@gmail.com>
2020-12-12 18:31:51 +0000

    added substitute system in sh for title

--- www/build.sh
+++ www/build.sh
@@ -1,8 +1,7 @@
+cat src/header.html | sed ´s/_Title_/Home > home.html
+cat src/home.html >> home.html
+cat src/footer.html >> home.html
 
-cat src/header.html > index.html
-cat src/home.html >> index.html
-cat src/footer.html >> index.html
-
-cat src/header.html > index.html
-cat src/cv.html >> index.html
-cat src/footer.html >> index.html
+cat src/header.html | sed ´s/_Title_/CV > cv.html
+cat src/cv.html >> cv.html
+cat src/footer.html >> cv.html
