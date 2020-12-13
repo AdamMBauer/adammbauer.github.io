13f4ebaa4e470cf2e4645e753bd68c3234ff4f2a
ElidorAzzeal <bauer.miles.adam@gmail.com>
2020-12-13 18:15:49 +0000

    added content to hobbies, cv and added nav section css + minor changes

--- www/build - Copy.sh
+++ www/build - Copy.sh
@@ -1,27 +1,19 @@
-cat src/header.html | sed ´s/__TITLE__/Home/´ > home.html
-
 cat src/header.html > home.html
-cat src/nav.html >> home.html
+cat src/nav.html > home.html
 cat src/home.html >> home.html
 cat src/footer.html >> home.html
 
-cat src/header.html | sed ´s/__TITLE__/CV/´ > cv.html
-
 cat src/header.html > cv.html
-cat src/nav.html >> cv.html
+cat src/nav.html > cv.html
 cat src/cv.html >> cv.html
 cat src/footer.html >> cv.html
 
-cat src/header.html | sed ´s/__TITLE__/CV/´ > projects.html
-
 cat src/header.html > projects.html
-cat src/nav.html >> projects.html
+cat src/nav.html > projects.html
 cat src/projects.html >> projects.html
 cat src/footer.html >> projects.html
 
-cat src/header.html | sed ´s/__TITLE__/CV/´ > hobbies.html
-
 cat src/header.html > hobbies.html
-cat src/nav.html >> hobbies.html
+cat src/nav.html > hobbies.html
 cat src/hobbies.html >> hobbies.html
 cat src/footer.html >> hobbies.html
