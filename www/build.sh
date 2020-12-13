a32d021289645ff6bd60bf82403f338e0e71c4f2
ElidorAzzeal <bauer.miles.adam@gmail.com>
2020-12-13 15:40:29 +0000

    add project page with simple description of itś purpose

--- www/build.sh
+++ www/build.sh
@@ -1,7 +1,7 @@
-cat src/header.html | sed ´s/_Title_/Home > home.html
+cat src/header.html | sed ´s/__TITLE__/Home/´ > home.html
 cat src/home.html >> home.html
 cat src/footer.html >> home.html
 
-cat src/header.html | sed ´s/_Title_/CV > cv.html
+cat src/header.html | sed ´s/__TITLE__/CV/´ > cv.html
 cat src/cv.html >> cv.html
 cat src/footer.html >> cv.html
