68337673bbd370eee11e64db95382b5e224efff5
ElidorAzzeal <bauer.miles.adam@gmail.com>
2020-12-17 11:10:09 +0000

    added model viewer

--- www/build.sh
+++ www/build.sh
@@ -1,27 +1,19 @@
-cat src/header.html | sed ´s/__TITLE__/Home/´ > home.html
+cat src/header.html | sed 's/__TITLE__/Home/g' > index.html
+cat src/home.html >> index.html
+cat src/footer.html >> index.html
 
-cat src/header.html > home.html
-cat src/nav.html >> home.html
-cat src/home.html >> home.html
-cat src/footer.html >> home.html
-
-cat src/header.html | sed ´s/__TITLE__/CV/´ > cv.html
-
-cat src/header.html > cv.html
-cat src/nav.html >> cv.html
+cat src/header.html | sed 's/__TITLE__/CV/g' > cv.html
 cat src/cv.html >> cv.html
 cat src/footer.html >> cv.html
 
-cat src/header.html | sed ´s/__TITLE__/CV/´ > projects.html
-
-cat src/header.html > projects.html
-cat src/nav.html >> projects.html
+cat src/header.html | sed 's/__TITLE__/Projects/g' > projects.html
 cat src/projects.html >> projects.html
 cat src/footer.html >> projects.html
 
-cat src/header.html | sed ´s/__TITLE__/CV/´ > hobbies.html
-
-cat src/header.html > hobbies.html
-cat src/nav.html >> hobbies.html
+cat src/header.html | sed 's/__TITLE__/Hobbies/g' > hobbies.html
 cat src/hobbies.html >> hobbies.html
 cat src/footer.html >> hobbies.html
+
+cat src/header.html | sed 's/__TITLE__/Models/g' > models.html
+cat src/models.html >> models.html
+cat src/footer.html >> models.html
